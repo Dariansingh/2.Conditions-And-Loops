6f9d9680c079b7f71e5cdba299be7f83cc1dff81
Dariansingh <53981547+Dariansingh@users.noreply.github.com>
2022-10-14 17:11:39 -0500

    commit

---
+++ VariablesAndOperations.js
@@ -0,0 +1,8 @@
+let username = prompt('Username:');
+let password = prompt('password:');
+
+if (username == 'samm123' && password == '12345') {
+    alert('welcome back, '+ username);
+} else {
+    alert('incorrect username or password.');
+}
